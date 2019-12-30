bb0dc5e1424088bca8d27c87e040378d27c04c15
lwngt <>
2019-12-30 21:12:09 +0900

    [modify]delete Japanese comment

--- app/src/test/delete_vendor_test.js
+++ app/src/test/delete_vendor_test.js
@@ -343,35 +343,3 @@ describe('Post /delete_project', function () {
   });
 
 });
-
-// # data & file recovery
-
-// cp /Users/eiden/Documents/workspaces/web/appdistributer/app/src/test/files/1566651445_03a891092c104de685119f0e50a8e981_1566651445_b1dfd064d14b4d02a47d3953e87892d7_b.jpg /Users/eiden/Documents/workspaces/web/appdistributer/app/src/public/images/screenshot/1566651445_03a891092c104de685119f0e50a8e981_1566651445_b1dfd064d14b4d02a47d3953e87892d7_b.jpg
-// cp /Users/eiden/Documents/workspaces/web/appdistributer/app/src/test/files/1567258827_18c288b9854a4437a9c2141fa3c3eaaf_app-debug.png /Users/eiden/Documents/workspaces/web/appdistributer/app/src/public/images/icon/1567258827_18c288b9854a4437a9c2141fa3c3eaaf_app-debug.png
-// cp /Users/eiden/Documents/workspaces/web/appdistributer/app/src/test/files/1567258827_18c288b9854a4437a9c2141fa3c3eaaf_app-debug.apk /Users/eiden/Documents/workspaces/web/appdistributer/app/src/public/files/apk/1567258827_18c288b9854a4437a9c2141fa3c3eaaf_app-debug.apk
-
-
-// INSERT INTO `vendors` (`id`, `name`, `api_key`, `contractor_vendor_id`, `country`, `province`, `city`, `address`, `zip_code`, `tel`, `created`, `updated`, `unregisted`)
-// VALUES
-// 	(2, '36B6492C4B858D3F9EF6C632E6D34B429F7672AD30CF7BEBC80B215B6D87AACE', 'b4defeca6dc3e360d766c7cd0efbce778164e6c3776bdfe8d887e91f', 3, '5A2A985684F7B734F195546BFDC0215F', '3F55BB0ACA6BA8466C0CCC712D51E5EF', '6F6A7BA1DCDE4342C96EC0D2C82534AC', '1D5A5E9D03413A9C22C001319B14ED47A7F229AC8187141E281994C18432103E', 'BEB4B2FDFB2B401AB6B2DA098FBCE685', '3E963EF407D5C8FE9CC388D69AA79482', 1, '2019-09-16 15:06:28', NULL, NULL);
-
-// INSERT INTO `users` (`id`, `vendor_id`, `first_name`, `last_name`, `authority_type`, `name`, `email`, `password`, `secret_key`, `created`, `updated`, `unregisted`)
-// VALUES
-// 	(3, 2, 'C8A3448BE132BF92F9C290C476710377', '9AC4AD926610C5266DF85889FDBD9FB4', 0, 'tester1122', '71CC4FCB86A6CE39540FD7766B75273DB3D9A71CA45F2620CEEB18377207152F', 'D5B7AA955E441056E4E9F5F15A95EEE1', '67f1a84f8c3589e570742fffefd22f229fdc3e88ba9e852a9164146eaaaa', '2019-05-15 12:27:10', NULL, NULL);
-
-// INSERT INTO `projects` (`id`, `vendor_id`, `name`, `image_1`, `image_2`, `image_3`, `image_4`, `image_5`, `overview`, `created`, `updated`, `unregisted`)
-// VALUES
-// 	(10, 2, 'test project', '1566651445_03a891092c104de685119f0e50a8e981_1566651445_b1dfd064d14b4d02a47d3953e87892d7_b.jpg', NULL, NULL, NULL, NULL, 'これはテストプロジェクtp', '2019-08-24 12:57:25', NULL, NULL);
-
-// INSERT INTO `user_manage_projects` (`id`, `vendor_id`, `vendor_id`, `project_id`, `created`, `updated`, `unregisted`)
-// VALUES
-// 	(19, 3, 2, 10, '2019-08-24 12:57:25', NULL, NULL);
-
-// INSERT INTO `buildings` (`id`, `vendor_id`, `project_id`, `upload_vendor_id`, `kind`, `app_name`, `icon`, `current_version`, `min_os_version`, `bundle_id`, `package_name`, `file_name`, `original_file_name`, `plist_file_name`, `release_note`, `enabled`, `created`, `updated`, `unregisted`)
-// VALUES
-// 	(27, 2, 10, 3, 1, 'テストアプリ', '1567258827_18c288b9854a4437a9c2141fa3c3eaaf_app-debug.png', '1.0', 5, NULL, 'com.example.myapplication', '1567258827_18c288b9854a4437a9c2141fa3c3eaaf_app-debug.apk', 'app-debug.apk', NULL, 'テスト用のアプリを追加', 1, '2019-08-31 13:40:37', NULL, NULL);
-
-// INSERT INTO `reports` (`id`, `report_building_id`, `report_vendor_id`, `note`, `created`, `updated`, `unregisted`)
-// VALUES
-// 	(20, 27, 3, 'fdasasfsa', '2019-09-05 13:12:50', NULL, NULL);
-
