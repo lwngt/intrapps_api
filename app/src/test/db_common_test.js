87e64f1dbaddccba2fcd33350014cead748da0b2
lwngt <>
2020-01-05 23:54:57 +0900

    [modify]delete no used json file, and rename mysql.conf to mysql.json

--- app/src/test/db_common_test.js
+++ app/src/test/db_common_test.js
@@ -7,7 +7,7 @@ let db_connection_func = new db_connection();
 
 describe("DB Connection test", () => {
   it("select * from files", () => {
-    let json = JSON.parse(fs.readFileSync('./db_conf/mysql.conf', 'utf8'));
+    let json = JSON.parse(fs.readFileSync('./db_conf/mysql.json', 'utf8'));
     // async function is this way.
     db_connection_func.execute_sql("select * from files;", json).then(result => {
       assert.isObject(result, 'either `null` or `undefined`');
@@ -15,7 +15,7 @@ describe("DB Connection test", () => {
   });
 
   it("execute wrong sql", () => {
-    let json = JSON.parse(fs.readFileSync('./db_conf/mysql.conf', 'utf8'));
+    let json = JSON.parse(fs.readFileSync('./db_conf/mysql.json', 'utf8'));
     // async function is this way.
     db_connection_func.execute_sql("select * from filesaaaa;", json).then(result => {
       assert.isNull(result, 'result is neither `null` nor `undefined`');
